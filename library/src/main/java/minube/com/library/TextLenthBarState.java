97926f64126470bdd373b4a8cdcd4905ff57313b
Francisco Alface Martin <franciscoalfacemartin@gmail.com>
2017-03-07 16:10:27 +0100

    Implemented TextLenthBarState

---
+++ library/src/main/java/minube/com/library/TextLenthBarState.java
@@ -0,0 +1,62 @@
+package minube.com.library;
+
+/**
+ * Created by franciscoalfacemartin on 7/3/17.
+ */
+
+public class TextLenthBarState {
+
+    protected int icon;
+    protected String text;
+    protected int charsLimit;
+    protected int backgroundColor;
+
+    private TextLenthBarState(Builder builder) {
+        icon = builder.icon;
+        text = builder.text;
+        charsLimit = builder.charsLimit;
+        backgroundColor = builder.backgroundColor;
+    }
+
+    public static final class Builder {
+        private int icon;
+        private String text;
+        private int charsLimit;
+        private int backgroundColor;
+
+        public Builder(int charsLimit, String text) {
+            this.charsLimit = charsLimit;
+            this.text = text;
+        }
+
+        public Builder icon(int val) {
+            icon = val;
+            return this;
+        }
+
+        public Builder backgroundColor(int val) {
+            backgroundColor = val;
+            return this;
+        }
+
+        public TextLenthBarState build() {
+            return new TextLenthBarState(this);
+        }
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getCharsLimit() {
+        return charsLimit;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+}
