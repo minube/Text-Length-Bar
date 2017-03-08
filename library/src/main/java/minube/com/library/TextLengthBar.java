fe27f5dc9fc4a2b996f3e11fa77de293ecea80d7
Paco Minube <pacominube@MacBook-Pro-de-Paco.local>
2017-03-08 12:18:10 +0100

    Added minChars attr

--- library/src/main/java/minube/com/library/TextLengthBar.java
+++ library/src/main/java/minube/com/library/TextLengthBar.java
@@ -3,6 +3,7 @@ package minube.com.library;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -30,7 +31,9 @@ public class TextLengthBar extends RelativeLayout {
     private ImageView imageView;
     private RelativeLayout rootView;
 
-    private List<TextLenthBarState> states;
+    private List<TextLengthBarState> states;
+    private TextLengthBarState currentState;
+    private int minChars;
 
     public TextLengthBar(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -46,13 +49,15 @@ public class TextLengthBar extends RelativeLayout {
 
     private void setupAttrs(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Tlb);
-        textSize = typedArray.getDimension(R.styleable.Tlb_barMessageTextSize, 16);
+        textSize = typedArray.getDimension(R.styleable.Tlb_barMessageTextSize, 12);
 
         textColor = typedArray.getColor(R.styleable.Tlb_barMessageTextColor,
             ContextCompat.getColor(context, android.R.color.white));
 
         backgroundColor = typedArray.getColor(R.styleable.Tlb_barBackgroundColor,
             ContextCompat.getColor(context, android.R.color.holo_blue_dark));
+
+        minChars = typedArray.getInt(R.styleable.Tlb_barMinChars, 1);
 
         content = typedArray.getString(R.styleable.Tlb_barMessage);
 
@@ -65,9 +70,13 @@ public class TextLengthBar extends RelativeLayout {
         inflate(getContext(), R.layout.text_length_bar, this);
         loadViews();
 
+        fillViewsWithContent(0, minChars);
+    }
+
+    private void fillViewsWithContent(int count, int minChars) {
         imageView.setImageDrawable(icon);
 
-        message.setText(content);
+        message.setText(content.replace("%d", String.valueOf(minChars - count)));
         message.setTextColor(textColor);
         message.setTextSize(textSize);
 
@@ -80,28 +89,55 @@ public class TextLengthBar extends RelativeLayout {
         rootView = (RelativeLayout) findViewById(R.id.root_view);
     }
 
-    public void setStates(List<TextLenthBarState> states) {
+    public void setStates(@NonNull List<TextLengthBarState> states) {
         this.states = states;
         Collections.sort(this.states);
     }
 
-    public void atachToEditText(EditText editText) {
+    public void attachToEditText(final EditText editText) {
         editText.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
             @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
-                for (TextLenthBarState state : states) {
-                    if (state.getCharsLimit() <= count) {
-                        break;
-                    }
-                }
+                int textLength = editText.getText().length();
+                currentState = getCurrentState(textLength);
+                updateContentWithState(textLength);
             }
 
             @Override public void afterTextChanged(Editable s) {
 
             }
         });
+    }
+
+    private void updateContentWithState(int count) {
+        if (currentState != null) {
+            int minCharsState = currentState.getCharsLimit();
+            message.setText(currentState.getText().toString().replace("%d", String.valueOf(minCharsState - count)));
+            rootView.setBackgroundColor(ContextCompat.getColor(getContext(), currentState.getBackgroundColor()));
+            imageView.setImageDrawable(ContextCompat.getDrawable(getContext(), currentState.getIcon()));
+        }
+    }
+
+    private TextLengthBarState getCurrentState(int count) {
+        for (int i = 0; states != null && i < states.size(); i++) {
+            if (i == 0) {
+                if (count > minChars && count < states.get(0).getCharsLimit()) {
+                    return states.get(0);
+                }
+            } else if (i > 0 && i < (states.size() - 1)) {
+                if (count >= states.get(i - 1).getCharsLimit() && count < states.get(i).getCharsLimit()) {
+                    return states.get(i);
+                }
+            } else {
+                if (count >= states.get(i).getCharsLimit()) {
+                    return states.get(i);
+                }
+            }
+        }
+        fillViewsWithContent(count, minChars);
+        return null;
     }
 }
