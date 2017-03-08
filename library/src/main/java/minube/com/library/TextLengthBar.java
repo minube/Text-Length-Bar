fb7d6f2102692350db1347d26695531977c28356
Paco Minube <pacominube@MacBook-Pro-de-Paco.local>
2017-03-08 16:06:45 +0100

    Added license headers

--- library/src/main/java/minube/com/library/TextLengthBar.java
+++ library/src/main/java/minube/com/library/TextLengthBar.java
@@ -1,3 +1,27 @@
+/*
+    MIT License
+
+    Copyright (c) 2017 minube
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
 package minube.com.library;
 
 import android.animation.ObjectAnimator;
@@ -69,6 +93,7 @@ public class TextLengthBar extends RelativeLayout {
         icon = typedArray.getDrawable(R.styleable.Tlb_barIcon);
 
         textFontPath = typedArray.getString(R.styleable.Tlb_textFontPath);
+
     }
 
     private void setupViews() {
@@ -84,7 +109,8 @@ public class TextLengthBar extends RelativeLayout {
         message.setText(content.replace("%d", String.valueOf(minChars - count)));
         message.setTextColor(textColor);
         message.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-        Typeface typeFace=Typeface.createFromAsset(getContext().getApplicationContext().getAssets(),textFontPath);
+
+        Typeface typeFace = Typeface.createFromAsset(getContext().getApplicationContext().getAssets(), textFontPath);
         message.setTypeface(typeFace);
 
         rootView.setBackgroundColor(backgroundColor);
